18a9d6bf7579fb674de8649e75533ed438a48c9b
rammium-droid <74028894+rammium-droid@users.noreply.github.com>
2023-08-01 18:27:23 -0700

    Logs the user out of their account

---
+++ CS157AProjectFrontend/LogoutServlet.java
@@ -0,0 +1,41 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession(false);
+		session.invalidate();
+		response.sendRedirect("login.html");
+		/* The two lines above make it so that once the user clicks "Log Out," the session is invalidated and they are sent back to 
+		the login page. This is done so that the user cannot return to their profile unless they log in again. */
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
